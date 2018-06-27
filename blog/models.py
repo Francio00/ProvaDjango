32163bae3db4fbc738649d53746602a8397f2b99
Francio00 <francheisy655@gmail.com>
2018-06-27 17:12:22 +0200

    Aggiornamento blog,creazione commenti,fix di vari errori

--- blog/models.py
+++ blog/models.py
@@ -9,10 +9,22 @@ class Post(models.Model):
     text=models.TextField()
     create_date = models.DateTimeField( default=timezone.now )
     published_date = models.DateTimeField( blank=True,null=True)
+    text2=models.TextField( blank=True,null=True)
+
 
     def publish(self):
         self.published_date=timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.title+" "+str(self.create_date)
+
+class Comments(models.Model):
+    """docstring for ClassName"""
+    post=models.ForeignKey('Post',on_delete=models.CASCADE)
+    published_date = models.DateTimeField(default=timezone.now)
+    comment=models.TextField(blank=True,null=True)
+    author=models.CharField(max_length=50,blank=True,default="anonymous")
+
+    def __str__(self):
+        return self.published_date+" "+self.author
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
-from django.shortcuts import render, get_object_or_404
+from .models import Post, Comments
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 def post_list(request):
@@ -10,4 +10,63 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comment_list= Comments.objects.filter(post=post)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post=post
+            comment.save()
+    else:
+        form = CommentForm()
+
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'comment_list' : comment_list, 'form':form})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form,'postID': post.pk})
+
+def post_delete(request, pk):
+    #cancello il post
+    post=get_object_or_404(Post,pk=pk)
+    post.delete()
+    return render(request,'blog/deleted_post.html',{})
+
+def comment_new(request):
+    if request.method == "POST":
+        form = CommentForm(request.COMMENT)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = "authorless"
+            comment.published_date = timezone.now()
+            comment.save()
+            #return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    #return render(request, 'blog/post_edit.html', {'form': form})
